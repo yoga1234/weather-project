588609e3ec176738c5b5a5aa1eeba0c315ea4d7f
Septiana Yoga <septianayoga30@gmail.com>
2022-09-28 09:16:13 +0700

    indexing

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import Cloud from "./cloud.jpg";
+import Daylight from "./daylight.jpg";
+import Nightlight from "./nightlight.jpg";
+import Rain from "./rain.jpg";
+
+export { Cloud, Daylight, Nightlight, Rain };
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Details from "./details/Details";
+import Forecast from "./forecast/Forecast";
+import Hero from "./hero/Hero";
+import Location from "./location/Location";
+import WeatherMain from "./weatherMain/WeatherMain";
+
+export { Details, Forecast, Hero, Location, WeatherMain };
