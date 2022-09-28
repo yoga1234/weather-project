2b0faf3ae1aadcf0c92d261dfe30fab82d941965
Septiana Yoga <septianayoga30@gmail.com>
2022-09-28 11:40:03 +0700

    basic style for weather details

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Details.css";
 
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <div className="details-container">
+      <h3>Weather Details</h3>
+      <div>
+        <p>
+          Cloud: <span>20%</span>
+        </p>
+        <p>
+          Humidity: <span>78%</span>
+        </p>
+        <p>
+          Wind: <span>1km/h</span>
+        </p>
+        <p>
+          Rain: <span>0mm</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -14,11 +14,6 @@ const Location = () => {
         <p>Location Found</p>
         <p>Location Found</p>
         <p>Location Found</p>
-        <p>Location Found</p>
-        <p>Location Found</p>
-        <p>Location Found</p>
-        <p>Location Found</p>
-        <p>Location Found</p>
       </div>
     </div>
   );
