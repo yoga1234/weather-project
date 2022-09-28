40d3059f92a368039589bb2b540a711ce7403cc6
Septiana Yoga <septianayoga30@gmail.com>
2022-09-28 12:49:09 +0700

    adding basic styles for forecast

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Forecast.css";
 
 const Forecast = () => {
-  return <div>Forecast</div>;
+  return (
+    <div className="forecast-container">
+      <h3>Forecast</h3>
+      <div>
+        <div className="forecast-item">
+          <p>Friday 09</p>
+          <p>Rain</p>
+        </div>
+        <div className="forecast-item">
+          <p>Friday 09</p>
+          <p>Rain</p>
+        </div>
+        <div className="forecast-item">
+          <p>Friday 09</p>
+          <p>Rain</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Forecast;
